b8406004c5ad2323e7e21b3e8ba965fa36fe6e79
hs92 <himanshu84207@gmail.com>
2020-05-16 22:35:58 +0530

    Created new project.

--- demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
+++ demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -1,13 +1,13 @@
-package com.example.New_project;
+package com.example.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewProjectApplication {
+public class DemoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NewProjectApplication.class, args);
+		SpringApplication.run(DemoappApplication.class, args);
 	}
 
 }
--- demoapp/src/test/java/com/example/demoapp/DemoappApplicationTests.java
+++ demoapp/src/test/java/com/example/demoapp/DemoappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.New_project;
+package com.example.demoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewProjectApplicationTests {
+class DemoappApplicationTests {
 
 	@Test
 	void contextLoads() {
