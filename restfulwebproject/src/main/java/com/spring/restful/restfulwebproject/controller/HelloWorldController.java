f1f6d8462875ac292a6a406ac4492d6cbf184340
hs92 <himanshu84207@gmail.com>
2020-05-24 13:35:50 +0530

    Refactoring - i18n concepts.

--- restfulwebproject/src/main/java/com/spring/restful/restfulwebproject/controller/HelloWorldController.java
+++ restfulwebproject/src/main/java/com/spring/restful/restfulwebproject/controller/HelloWorldController.java
@@ -1,11 +1,9 @@
 package com.spring.restful.restfulwebproject.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +20,7 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping("/hello-world-international")
-	public String helloWorldInternational(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternational() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
